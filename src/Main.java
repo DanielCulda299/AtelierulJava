4342aaa88752d68a192510fcd0f691ef51f5d57f
Daniel Culda <danieldani0430@gmail.com>
2018-11-12 00:03:56 +0200

    - 6 commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[6];
+        System.out.print("Please enter " + numbers.length + " values:");
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = input.nextInt();
+        }
+        input.close();
+        System.out.print("Values is ");
+        for (int n : numbers) {
+            System.out.print(n + "\t");
+        }
     }
 }
